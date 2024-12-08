829547eb7b3b5763a02b9dfb1d88cd6949747513
sewminiDeSilva <imesha20005@gmail.com>
2024-12-08 22:30:05 +0530

    Update TextBoxExample.java file

--- src/test/java/TextBoxExample.java
+++ src/test/java/TextBoxExample.java
@@ -10,32 +10,32 @@ public class TextBoxExample {
    WebDriver driver;
     @BeforeMethod
     public  void GoogleTest(){
-        WebDriver driver =new ChromeDriver();
-        driver.get("");
+        driver =new ChromeDriver();
+        driver.get("https://www.leafground.com/input.xhtml");
     }
 
    @Test
    public void textBoxTest(){
 
        //type your  name
-       WebElement name = driver.findElement(By.id(""));
+       WebElement name = driver.findElement(By.id("j_idt88:name"));
        name.sendKeys("sewmini");
 
        //verify if text box is disable
-       boolean enabled = driver.findElement(By.name("")).isEnabled();
+       boolean enabled = driver.findElement(By.name("j_idt88:j_idt93")).isEnabled();
        System.out.println("Is enable"+ enabled);
 
        //clear the typed text
-       WebElement clearText = driver.findElement(By.xpath(""));
+       WebElement clearText = driver.findElement(By.xpath("//input[@id='j_idt88:j_idt95']"));
        clearText.clear();
 
        //retrieve  the type text
-       WebElement textElement = driver.findElement(By.id(""));
+       WebElement textElement = driver.findElement(By.id("j_idt88:j_idt97"));
        String value =textElement.getAttribute("value");
        System.out.println(value);
 
        //Type email &  tab. confirm control moved to next element
-       driver.findElement(By.id("")).sendKeys("sample@gmail.com" + Keys.TAB + "control moved to next element");
+       driver.findElement(By.id("j_idt88:j_idt99")).sendKeys("sample@gmail.com" + Keys.TAB + "control moved to next element");
    }
 
 }
